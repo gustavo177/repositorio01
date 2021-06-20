354edec7eb3eea8b753d0a41d6ae58529b662b75
gustavo177 <39227407+gustavo177@users.noreply.github.com>
2021-06-20 16:51:49 -0500

    commit version 2  mi aporte "version 2" en el proyecto.java

--- src/proyecto/Proyecto.java
+++ src/proyecto/Proyecto.java
@@ -16,6 +16,8 @@ public class Proyecto {
      */
     public static void main(String[] args) {
         System.out.println("Hola Mundo!");
+        System.out.println("versión 2");
+        
     }
     
 }
